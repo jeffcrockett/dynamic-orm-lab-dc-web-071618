e6ea94d0f86d1ba6552fad1b49f399bbc6eb2fc0
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-30 12:56:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,27 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
-
+require 'pry'
 class Student < InteractiveRecord
 
+  def initialize(props={})
+    @id = props[:id]
+    @name = props[:name]
+    @grade = props[:grade]
+  end
+
+  self.column_names.each do |col|
+    attr_accessor col.to_sym
+  end
+
+  def self.find_by(attr: value)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attr} = #{value}"
+    DB[:conn].execute(sql)
+
+  end
+  binding.pry
+  # def self.find_by(name)
+  #   self.find_by_name(name)
+  # end
 end
+# binding.pry
